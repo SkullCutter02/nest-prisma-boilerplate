24437d9c1d901b2d8840d92e17c2209b54734e6a
skullcutter02 <=>
2022-01-17 16:19:54 +0800

    Created get post route

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseUUIDPipe, Post, UseGuards } from "@nestjs/common";
 import { User } from "@prisma/client";
 
 import { PostService } from "./post.service";
@@ -9,6 +9,16 @@ import { GetUser } from "../decorators/getUser.decorator";
 @Controller("post")
 export class PostController {
   constructor(private readonly postService: PostService) {}
+
+  @Get("/:id")
+  getPost(@Param("id", ParseUUIDPipe) postId: string) {
+    return this.postService.getPost(postId);
+  }
+
+  @Get()
+  getPosts() {
+    return this.postService.getPosts();
+  }
 
   @Post()
   @UseGuards(JwtAuthGuard)
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -8,6 +8,14 @@ import { PrismaService } from "../prisma/prisma.service";
 export class PostService {
   constructor(private readonly prisma: PrismaService) {}
 
+  getPost(postId: string) {
+    return this.prisma.post.findUnique({ where: { id: postId }, include: { author: true } });
+  }
+
+  getPosts() {
+    return this.prisma.post.findMany({ include: { author: true } });
+  }
+
   createPost(createPostDto: CreatePostDto, user: User) {
     return this.prisma.post.create({
       data: {
