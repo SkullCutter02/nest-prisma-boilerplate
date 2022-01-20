e6c2e86e2161f01077da382b7530c8a7aa28f17c
skullcutter02 <=>
2022-01-20 22:26:01 +0800

    Commented out post

--- src/post/dto/createPost.dto.ts
+++ src/post/dto/createPost.dto.ts
@@ -1,10 +1,10 @@
-import { IsString, MaxLength } from "class-validator";
-
-export class CreatePostDto {
-  @IsString()
-  @MaxLength(500)
-  title: string;
-
-  @IsString()
-  body: string;
-}
+// import { IsString, MaxLength } from "class-validator";
+//
+// export class CreatePostDto {
+//   @IsString()
+//   @MaxLength(500)
+//   title: string;
+//
+//   @IsString()
+//   body: string;
+// }
--- src/post/dto/editPost.dto.ts
+++ src/post/dto/editPost.dto.ts
@@ -1,5 +1,5 @@
-import { PartialType } from "@nestjs/mapped-types";
-
-import { CreatePostDto } from "./createPost.dto";
-
-export class EditPostDto extends PartialType(CreatePostDto) {}
+// import { PartialType } from "@nestjs/mapped-types";
+//
+// import { CreatePostDto } from "./createPost.dto";
+//
+// export class EditPostDto extends PartialType(CreatePostDto) {}
